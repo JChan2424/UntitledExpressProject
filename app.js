96f0af5f30bfc7a5c396a274b6a3943476a3c908
Joshua Chan <jchan@Joshuas-MacBook-Pro.local>
2023-06-21 23:55:48 -0700

    Added webpack and edited app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express();
+const server = app.listen(process.env.PORT || 8080, ()=>console.log("Listening"));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
